8c95b6287616b6b7b51d4b533e1c871980aca98f
Matteo Ravasi <ravasim@vsc509-03-l.ibex.kaust.edu.sa>
2023-07-14 22:38:55 +0300

    Cleaned inversiondist submodule

--- mdctlr/inversiondist/mdd_mixedprec.py
+++ mdctlr/inversiondist/mdd_mixedprec.py
@@ -1,15 +1,16 @@
 import logging
 import numpy as np
 
-from pylops import Identity, Transpose
+from pylops import Identity
 from pylops.signalprocessing import FFT
 from .Fredholm1_mixedprec import Fredholm1mixed
 
-def _MDC(nt, nv, dt=1., dr=1., twosided=True, fast=None, dtype=None,
-         transpose=True, saveGt=True, conj=False, prescaled=False,
-         _Identity=Identity, _Transpose=Transpose, _FFT=FFT,
-         _Fredholm1=Fredholm1mixed, args_Identity={}, args_Transpose={},
-         args_FFT={}, args_Identity1={}, args_Transpose1={},
+
+def _MDC(nt, nv, dt=1., dr=1., twosided=True, dtype=None,
+         conj=False, prescaled=False,
+         _Identity=Identity, _FFT=FFT,
+         _Fredholm1=Fredholm1mixed, args_Identity={},
+         args_FFT={}, args_Identity1={}, 
          args_FFT1={}, args_Fredholm1={}):
     r"""Multi-dimensional convolution.
 
@@ -20,14 +21,6 @@ def _MDC(nt, nv, dt=1., dr=1., twosided=True, fast=None, dtype=None,
     operator.
 
     """
-    # warnings.warn('A new implementation of MDC is provided in v1.5.0. This '
-    #               'currently affects only the inner working of the operator, '
-    #               'end-users can continue using the operator in the same way. '
-    #               'Nevertheless, it is now recommended to start using the '
-    #               'operator with transpose=True, as this behaviour will '
-    #               'become default in version v2.0.0 and the behaviour with '
-    #               'transpose=False will be deprecated.', FutureWarning)
-
     if twosided and nt % 2 == 0:
         raise ValueError('nt must be odd number')
 
@@ -64,63 +57,40 @@ def _MDC(nt, nv, dt=1., dr=1., twosided=True, fast=None, dtype=None,
     F1opH = F1op.H
     I1opH = I1op.H
 
-    # create transpose operator
-    if transpose:
-        dims = [nr, nt] if nv == 1 else [nr, nv, nt]
-        axes = (1, 0) if nv == 1 else (2, 0, 1)
-        Top = _Transpose(dims, axes, dtype=dtype, **args_Transpose)
-
-        dims = [nt, ns] if nv == 1 else [nt, ns, nv]
-        axes = (1, 0) if nv == 1 else (1, 2, 0)
-        TopH = _Transpose(dims, axes, dtype=dtype, **args_Transpose1)
-
     # create MDC operator
     MDCop = F1opH * I1opH * Frop * Iop * Fop
-    if transpose:
-        MDCop = TopH * MDCop * Top
-
+    
     # force dtype to be real (as FFT operators assume real inputs and outputs)
     MDCop.dtype = rdtype
 
     return MDCop
 
 
-def MDCmixed(TLRop, ns, nr, nt, nfreq, nv, dt=1., dr=1., twosided=True, fast=None,
-             dtype='complex64', fftengine='numpy', transpose=True,
-             saveGt=True, conj=False, prescaled=False,
+def MDCmixed(TLRop, ns, nr, nt, nfreq, nv, dt=1., dr=1., twosided=True,
+             dtype='complex64', fftengine='numpy',
+             conj=False, prescaled=False,
              nb=128, acc='0.001', datafolder='.'):
-    r"""Multi-dimensional convolution with mixed-precision TLR.
+    r"""Multi-dimensional convolution with mixed-precision TLR-MVM kernel.
 
-    Apply multi-dimensional convolution between two datasets. If
-    ``transpose=True``, model and data should be provided after flattening
-    2- or 3-dimensional arrays of size :math:`[n_r (\times n_{vs}) \times n_t]`
-    and :math:`[n_s (\times n_{vs}) \times n_t]` (or :math:`2*n_t-1` for
-    ``twosided=True``), respectively. If ``transpose=False``, model and data
-    should be provided after flattening 2- or 3-dimensional arrays of size
-    :math:`[n_t \times n_r (\times n_{vs})]` and
+    Apply multi-dimensional convolution between two datasets with mixed-precision 
+    TLR-MVM kernel. Model and data should be provided after flattening 2- or 
+    3-dimensional arrays of size :math:`[n_t \times n_r (\times n_{vs})]` and
     :math:`[n_t \times n_s (\times n_{vs})]` (or :math:`2*n_t-1` for
     ``twosided=True``), respectively.
 
-    .. warning:: A new implementation of MDC is provided in v1.5.0. This
-      currently affects only the inner working of the operator and end-users
-      can use the operator in the same way as they used to do with the previous
-      one. Nevertheless, it is now reccomended to use the operator with
-      ``transpose=False``, as this behaviour will become default in version
-      v2.0.0 and the behaviour with ``transpose=True`` will be deprecated.
-
     Parameters
     ----------
-    U : :obj:`numpy.ndarray`
-        Multi-dimensional convolution kernel U basis in frequency domain of size
-        :math:`[n_s \times n_r \times n_{fmax}]` if ``transpose=True``
-        or size :math:`[n_{fmax} \times n_s \times n_r]` if ``transpose=False``
-    V : :obj:`numpy.ndarray`
-        Multi-dimensional convolution kernel V basis in frequency domain of size
-        :math:`[n_s \times n_r \times n_{fmax}]` if ``transpose=True``
-        or size :math:`[n_{fmax} \times n_s \times n_r]` if ``transpose=False``
+    TLRop : :obj:`tlrmvm.tilematrix.TilematrixGPU_Ove3D`
+        TLR-MVM operator
+    ns : :obj:`int`
+        Number of samples along source axis
+    nr : :obj:`int`
+        Number of receiver along source axis
     nt : :obj:`int`
         Number of samples along time axis for model and data (note that this
         must be equal to ``2*n_t-1`` when working with ``twosided=True``.
+    nfreq : :obj:`int`
+        Number of frequencies
     nv : :obj:`int`
         Number of samples along virtual source axis
     dt : :obj:`float`, optional
@@ -130,33 +100,24 @@ def MDCmixed(TLRop, ns, nr, nt, nfreq, nv, dt=1., dr=1., twosided=True, fast=Non
     twosided : :obj:`bool`, optional
         MDC operator has both negative and positive time (``True``) or
         only positive (``False``)
-    fast : :obj:`bool`, optional
-        *Deprecated*, will be removed in v2.0.0
     dtype : :obj:`str`, optional
         *Deprecated*, will be removed in v2.0.0
     fftengine : :obj:`str`, optional
         Engine used for fft computation (``numpy`` or ``fftw``)
-    transpose : :obj:`bool`, optional
-        Transpose ``G`` and inputs such that time/frequency is placed in first
-        dimension. This allows back-compatibility with v1.4.0 and older but
-        will be removed in v2.0.0 where time/frequency axis will be required
-        to be in first dimension for efficiency reasons.
-    saveGt : :obj:`bool`, optional
-        Save ``G`` and ``G^H`` to speed up the computation of adjoint of
-        :class:`pylops.signalprocessing.Fredholm1` (``True``) or create
-        ``G^H`` on-the-fly (``False``) Note that ``saveGt=True`` will be
-        faster but double the amount of required memory
     conj : :obj:`str`, optional
         Perform Fredholm integral computation with complex conjugate of ``G``
-    usematmul : :obj:`bool`, optional
-        Use :func:`numpy.matmul` (``True``) or for-loop with :func:`numpy.dot`
-        (``False``) in :py:class:`pylops.signalprocessing.Fredholm1` operator.
-        Refer to Fredholm1 documentation for details.
     prescaled : :obj:`bool`, optional
         Apply scaling to kernel (``False``) or not (``False``) when performing
         spatial and temporal summations. In case ``prescaled=True``, the
         kernel is assumed to have been pre-scaled when passed to the MDC
         routine.
+    nb : :obj:`int`, optional
+        Tile size of TLR compression
+    acc : :obj:`str`, optional
+        Accuracy of TLR compression
+    datafolder : :obj:`str`, optional
+        Path of folder containing U and V bases for TLR-MVM
+
 
     Raises
     ------
@@ -203,8 +164,8 @@ def MDCmixed(TLRop, ns, nr, nt, nfreq, nv, dt=1., dr=1., twosided=True, fast=Non
 
     """
     
-    return _MDC(nt, nv, dt=dt, dr=dr, twosided=twosided, fast=fast,
-                dtype=dtype, transpose=transpose, saveGt=saveGt,
+    return _MDC(nt, nv, dt=dt, dr=dr, twosided=twosided,
+                dtype=dtype,
                 conj=conj, prescaled=prescaled,
                 args_FFT={'engine': fftengine},
                 args_Fredholm1={'TLRop':TLRop, 'nb':nb, 'acc':acc, 'nfreq':nfreq,
