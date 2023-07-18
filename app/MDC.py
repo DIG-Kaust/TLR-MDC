Fixed Fredholm1_mixedprec.py to include scaling

app/MDC.py
@@ -128,6 +128,7 @@ def main(parser):
     ######### LOAD AUXILIARY INPUTS (GEOMETRY, SUBSURFACE WAVEFIELDS, WAVELET) AND PREPARE FOR MCK #########
     inputfile_aux = join(STORE_PATH, args.AuxFile)
     inputdata_aux = np.load(inputfile_aux)
+    
     # Receivers
     r = inputdata_aux['recs'].T
     nr = r.shape[1]
@@ -137,11 +138,8 @@ def main(parser):
     # Virtual points
     vs = inputdata_aux['vs']
     # Time axis
-    #t = inputdata_aux['t']
-    #ot, dt, nt = t[0], t[1], len(t)
-    # Time axis
-    ot, dt, nt = 0, 2.5e-3, 601
-    t = np.arange(nt) * dt
+    t = inputdata_aux['t']
+    ot, dt, nt = t[0], t[1], len(t)
 
     # Find area of each volume - note that areas at the edges and on vertex are unbounded,
     # we will assume that they are and use the minimum are for all points in this example
@@ -160,6 +158,7 @@ def main(parser):
     G0sub = G0sub[wav_c:][:nt]
 
     # Rearrange inputs according to matrix re-arrangement
+    idx = None
     if args.OrderType != 'normal':
         idx = np.load(join(STORE_PATH, 'Mck_rearrangement.npy'))
         G0sub = G0sub[:, idx]
@@ -184,8 +183,8 @@ def main(parser):
         # Load TLR kernel
         mvmops = TilematrixGPU_Ove3D(args.M, args.N, args.nb, 
                                      synthetic=False, datafolder=join(STORE_PATH,'compresseddata'), 
-                                     order=args.OrderType, acc=args.threshold, 
-                                     freqlist=Ownfreqlist, suffix="Mck_freqslice_")
+                                     order=args.OrderType, srcidx=idx, recidx=idx,
+                                     acc=args.threshold, freqlist=Ownfreqlist, suffix="Mck_freqslice_")
         mvmops.estimategpumemory()
         mvmops.loaduvbuffer()
         mvmops.setcolB(1) # just 1 point

app/MDD.py
@@ -38,6 +38,7 @@ def main(parser):
     parser.add_argument("--ModeValue", type=int, default=8, help="Rank mode")
     parser.add_argument("--M", type=int, default=9801, help="Number of sources/rows in seismic frequency data")
     parser.add_argument("--N", type=int, default=9801, help="Number of receivers/columns in seismic frequency data")
+    parser.add_argument("--ivsinv", type=int, default=880, help="Index of virtual source to invert for")
     parser.add_argument("--nb", type=int, default=256, help="TLR Tile size")
     parser.add_argument("--threshold", type=str, default="0.001", help="TLR Error threshold")
 
@@ -129,20 +130,23 @@ def main(parser):
     inputdata_aux = np.load(inputfile_aux)
 
     # Sources
-    #s = inputdata_aux['srcs'].T
-    #ns = s.shape[1]
+    s = inputdata_aux['srcs'].T
+    ns = s.shape[1]
 
     # Virtual sources grid
-    vsz = 650
-    nvsx = 71
-    dvsx = 20
-    ovsx = 200
-    nvsy = 41
-    dvsy = 20
-    ovsy = 200
+    vsz = inputdata_aux['vsz']
+    vsx = inputdata_aux['vsx']
+    vsy = inputdata_aux['vsy']
+    nvsx = len(vsx)
+    dvsx = vsx[1] - vsx[0]
+    ovsx = vsx[0]
+    nvsy = len(vsy)
+    dvsy = vsy[1] - vsy[0]
+    ovsy = vsy[0]
     nvs = nvsx * nvsy
-    ivsx, ivsy = 21, 19
-    ivsinv = ivsx * nvsy + ivsy
+    #ivsx, ivsy = 21, 19
+    #ivsinv = ivsx * nvsy + ivsy
+    ivsinv = args.ivsinv
 
     # Time axis
     t = inputdata_aux['t']
@@ -284,4 +288,4 @@ if __name__ == "__main__":
 
 # TO DO:
 # - Dense
-# - TLR gives NaN!
+# - Hilbert

app/MarchenkoRedatuming.py
@@ -29,7 +29,7 @@ from pylops.basicoperators import Roll
 from mdctlr.inversiondist import MDCmixed
 from mdctlr.lsqr import lsqr
 from mdctlr.utils import voronoi_volumes
-from tlrmvm.tilematrix import TilematrixGPU_Ove3D
+from mdctlr.tlrmvm.tilematrix import TilematrixGPU_Ove3D
 
 
 def main(parser):
@@ -229,6 +229,7 @@ def main(parser):
         plt.savefig(join(TARGET_FIG_PATH, "analyticaldirectwave.jpg"))
 
     # Rearrange inputs according to matrix re-arrangement
+    idx = None
     if args.OrderType != 'normal':
         idx = np.load(join(STORE_PATH,'Mck_rearrangement.npy'))
         G0sub_ana_shuffled = G0sub_ana[:, idx]
@@ -274,7 +275,8 @@ def main(parser):
         # Load TLR kernel
         mvmops = TilematrixGPU_Ove3D(args.M, args.N, args.nb, 
                                      synthetic=False, datafolder=join(STORE_PATH,'compresseddata'), 
-                                     order=args.OrderType, acc=args.threshold, freqlist=Ownfreqlist, 
+                                     order=args.OrderType, srcidx=idx, recidx=idx,
+                                     acc=args.threshold, freqlist=Ownfreqlist, 
                                      suffix="Mck_freqslice_")
         mvmops.estimategpumemory()
         mvmops.loaduvbuffer()
@@ -291,9 +293,9 @@ def main(parser):
     comm.Barrier()
 
     dRop = MDCmixed(mvmops, ns, nr, nt=2*nt-1, nfreq=nfmax, nv=1, dt=dt, dr=2*darea, nb=args.nb,  acc=args.threshold,
-                    datafolder=join(STORE_PATH, 'compresseddata'), transpose=False, conj=False)
+                    datafolder=join(STORE_PATH, 'compresseddata'), conj=False)
     dR1op = MDCmixed(mvmops, ns, nr, nt=2*nt-1, nfreq=nfmax, nv=1, dt=dt, dr=2*darea, nb=args.nb,  acc=args.threshold,
-                     datafolder=join(STORE_PATH, 'compresseddata'), transpose=False, conj=True)
+                     datafolder=join(STORE_PATH, 'compresseddata'), conj=True)
     dRollop = Roll((2*nt-1) * nr,dims=(2*nt-1, nr),dir=0, shift=-1)
 
     dWop = Diagonal(w_shuffled.T.flatten())
@@ -316,14 +318,24 @@ def main(parser):
 
     if mpirank == 0 and args.debug:
         fig, ax = plt.subplots(1, 1, sharey=True, figsize=(16, 7))
-        ax.imshow(cp.asnumpy(dfd_plus), cmap='gray', extent=(0, nr, t[-1], -t[-1]))
+        im = ax.imshow(cp.asnumpy(dfd_plus), cmap='gray', extent=(0, nr, t[-1], -t[-1]))
         ax.set_title(r'$f_d^+$'), ax.set_xlabel(r'$x_R$'), ax.set_ylabel(r'$t$')
-        ax.axis('tight');
+        ax.axis('tight')
+        plt.colorbar(im)
         plt.savefig(join(TARGET_FIG_PATH, 'dfplus_shuffled.png'), bbox_inches='tight')
 
     # Create data
     dd = dWop*dRop*dfd_plus.flatten()
     dd = cp.concatenate((dd.reshape(2*nt-1, nr), cp.zeros((2*nt-1, nr), dtype=np.float32)))
+
+    if mpirank == 0 and args.debug:
+        fig, ax = plt.subplots(1, 1, sharey=True, figsize=(16, 7))
+        im = ax.imshow(cp.asnumpy(dd)[:2*nt-1], cmap='gray', extent=(0, nr, t[-1], -t[-1]))
+        ax.set_title(r'$d$'), ax.set_xlabel(r'$x_R$'), ax.set_ylabel(r'$t$')
+        ax.axis('tight')
+        plt.colorbar(im)
+        plt.savefig(join(TARGET_FIG_PATH, 'data_shuffled.png'), bbox_inches='tight')
+
 
     ######### RUN INVERSION #########
     if mpirank == 0: